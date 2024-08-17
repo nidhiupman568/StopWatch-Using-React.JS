34a8a610830a440df555e53ec2575033ffc0050f
nidhiupman568 <upmanyunidhi3092@gmail.com>
2024-08-17 10:47:09 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import StopWatch from './Components/StopWatch/StopWatch.jsx';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="App"><h1>StopWatch Using React.JS - By Nidhi Upman</h1>
 			<StopWatch />
 		</div>
 	);
